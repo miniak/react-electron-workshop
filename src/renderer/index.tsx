58c3505c772efc7b4f44577e6b0a243890cc21c6
Milan Burda <miburda@microsoft.com>
2018-10-30 08:42:07 +0100

    Gallery

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,11 +1,60 @@
+// electron
+import { remote } from 'electron';
+
+// node
+import * as fs from 'fs';
+import * as path from 'path';
+import * as url from 'url';
+
 // react
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+interface IGalleryItemProps {
+    filePath: string;
+}
+
+class GalleryItem extends React.Component<IGalleryItemProps> {
+    public render() {
+        return (
+            <img src={this._fileUrl} />
+        );
+    }
+
+    private get _fileUrl() {
+        return url.format({
+            protocol: 'file',
+            pathname: this.props.filePath,
+        });
+    }
+}
+
+interface IGalleryProps {
+    dirPath: string;
+}
+
+const kImageFilesPattern = /\.(jpg|png|gif)/i;
+
+class Gallery extends React.Component<IGalleryProps> {
+    public render() {
+        const dirPath = this.props.dirPath;
+        const files = fs.readdirSync(dirPath).filter(file => file.match(kImageFilesPattern));
+
+        return (<>
+            <h1>{dirPath}</h1>
+            <div>
+                {files.map((file) => (
+                    <GalleryItem key={file} filePath={path.join(dirPath, file)} />
+                ))}
+            </div>
+        </>);
+    }
+}
+
 class App extends React.Component {
     public render() {
         return (
-            <h1>Hello World!</h1>
+            <Gallery dirPath={remote.app.getPath('pictures')} />
         );
     }
 }
