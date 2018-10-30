312e51b40eaa86c27cd025c8a3963a9e1e2d37b9
Milan Burda <miburda@microsoft.com>
2018-10-30 08:49:14 +0100

    Select Directory

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -51,11 +51,39 @@ class Gallery extends React.Component<IGalleryProps> {
     }
 }
 
-class App extends React.Component {
+interface IAppState {
+    dirPath: string;
+}
+
+class App extends React.Component<{}, IAppState> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            dirPath: remote.app.getPath('pictures')
+        }
+
+        this._selectDirectory = this._selectDirectory.bind(this);
+    }
+
     public render() {
-        return (
-            <Gallery dirPath={remote.app.getPath('pictures')} />
-        );
+        return (<>
+            <button onClick={this._selectDirectory}>Select Directory</button>
+            <Gallery dirPath={this.state.dirPath} />
+        </>);
+    }
+
+    private _selectDirectory() {
+        const window = remote.getCurrentWindow();
+        remote.dialog.showOpenDialog(window, {
+            defaultPath: this.state.dirPath,
+            properties: ['openDirectory']
+        }, (filePaths) => {
+            if (!filePaths) return;
+            this.setState({
+                dirPath: filePaths[0]
+            });
+        });
     }
 }
 
